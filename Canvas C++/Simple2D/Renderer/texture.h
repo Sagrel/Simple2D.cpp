0ba86d68e707a63c6ee1a8e8e59d2f25ed27632d
Sagrel <31445649+Sagrel@users.noreply.github.com>
2018-10-22 17:54:55 +0200

    Update texture.h

--- Canvas C++/Simple2D/Renderer/texture.h
+++ Canvas C++/Simple2D/Renderer/texture.h
@@ -8,17 +8,12 @@ class Shape
 {
 public:
 	Shape(const std::string& path);
+	Shape(Shape&& s);
 	~Shape();
 
 	void Bind(unsigned int slot = 0) const;
 	void Unbind() const;
 
-	inline int GetWidth() const { return width; }
-	inline int GeHeight() const { return height; }
-
 private:
 	unsigned int RendererID;
-	std::string path;
-	unsigned char* localBuffer;
-	int width, height, BPP;
 };
